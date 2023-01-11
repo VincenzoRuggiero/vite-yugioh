577d09a439812a614614a1d4ab4bc6e4ef641ac4
Vincenzo Ruggiero <vincenzoru@live.it>
2023-01-11 17:13:14 +0100

    Code refactoring

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,40 +1,16 @@
 <script>
-//Importo Axios per recuperare i dati dall'API
-import axios from "axios";
-
-//Collego il file JS che contiene i dati delle carte
-import { store } from "../store.js";
+import CardComp from "./CardComp.vue";
 
 export default {
   name: "AppMain",
-
-  data() {
-    return {
-      store,
-    };
-  },
-
-  methods: {
-    //Recupero le informazioni per le carte tramite l'API fornita
-    getCards() {
-      axios
-        .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0")
-        .then((response) => {
-          this.store.cardsList = response.data.data;
-        });
-    },
-  },
-
-  created() {
-    this.getCards();
+  components: {
+    CardComp,
   },
 };
 </script>
 
 <template>
-  <p v-for="card in store.cardsList">
-    {{ card.name }}
-  </p>
+  <CardComp />
 </template>
 
 <style lang="scss"></style>
--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,11 +1,40 @@
 <script>
+//Importo Axios per recuperare i dati dall'API
+import axios from "axios";
+
+//Collego il file JS che contiene i dati delle carte
+import { store } from "../store.js";
+
 export default {
   name: "CardComp",
+
+  data() {
+    return {
+      store,
+    };
+  },
+
+  methods: {
+    //Recupero le informazioni per le carte tramite l'API fornita
+    getCards() {
+      axios
+        .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0")
+        .then((response) => {
+          this.store.cardsList = response.data.data;
+        });
+    },
+  },
+
+  created() {
+    this.getCards();
+  },
 };
 </script>
 
 <template>
-  <div></div>
+  <p v-for="card in store.cardsList">
+    {{ card.name }}
+  </p>
 </template>
 
 <style lang="scss"></style>
