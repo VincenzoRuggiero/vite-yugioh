6a626b647656fef752960f815887ad357c23c749
Vincenzo Ruggiero <vincenzoru@live.it>
2023-01-11 14:58:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import MainApp from "./components/MainApp.vue";
+
+export default {
+  components: {
+    MainApp,
+  },
+};
+</script>
+
+<template>
+  <p class="text-danger">Wella</p>
+</template>
+
+<style lang="scss">
+@use "../node_modules/bootstrap/scss/bootstrap.scss";
+</style>
